321e421b24e4dfe2aea96e6385c0dc334511e567
lumiz <lumiz@DESKTOP-OT4PSQN>
2023-10-12 11:55:09 +0200

    Add project files.

---
+++ Cappic.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cappic.Models;
+
+
+namespace Cappic.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
---
+++ Cappic.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Cappic.Models;
+
+namespace Cappic.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        
+    }
+}
